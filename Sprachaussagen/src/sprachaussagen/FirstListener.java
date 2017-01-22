dbee6c84d896a56853f32931c89799b047a33081
a-ya-mi <fattiavantia@gmail.com>
2017-01-22 22:08:31 +0100

    GUI Eingabe-Ausgabe + Test Audio

--- Sprachaussagen/src/sprachaussagen/FirstListener.java
+++ Sprachaussagen/src/sprachaussagen/FirstListener.java
@@ -14,9 +14,7 @@ import javax.swing.JLabel;
  * @author Aksana
  */
 public class FirstListener implements ActionListener {
-    //um JLabel von Klasse UnserFrame zu greifen, ändern
-    //wir definieren Variable und mit Konstruktor 
-    //weisen in diese Variable JLabel von UnserFrame
+   
     JLabel lbl;
     FirstListener (JLabel lbl){
         this.lbl=lbl;
